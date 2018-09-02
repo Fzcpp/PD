c08b6d55ebaf724a463dabdda77f42a32134f0b3
sunzhaohao <sunzhaohenan@yahoo.com>
2018-09-02 22:25:56 +0800

    Lua调用C#

---
+++ 2DShape/Assets/Scenes/LuaAndCSharp/test/testLuaCallCS.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testLuaCallCS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LuaCallCSharp callCS=new LuaCallCSharp();
+         
+	    Program pro = new Program();
+	    pro.GetRef();
+        string script = "";
+        object[] arguments=null;
+	   
+	    arguments = new object[] {  4, 265 };
+	    object result = callCS.CallStaticFunc(script, pro, "Add", arguments);
+	    print(result);
+        /////////////////////
+	    arguments = new object[] { 2344, 265 };
+       
+        ScriptFile scriptFile=new ScriptFile("firstScript.txt");
+        scriptFile.AddScript("print('hello world')");
+
+        result = callCS.CallFunction(scriptFile, pro, "Sub", arguments);
+        print(result);
+        //在调用C#函数之后调用lua脚本
+        scriptFile.AddScript("print('lua scipt:',result)");
+
+        callCS.CallLuaScript();
+        scriptFile.ClearAllScripts();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ 2DShape/Assets/Scenes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine  ;
+
+interface ICSObject
+{
+     object getRef();
+}
+    public class Program :ICSObject
+    {
+        public int count = 5;
+        public static double Add(double a, double b)
+        {
+            return  a + b;
+        }
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public double Double(double num)
+        {
+            return num * 2;
+        }
+
+        public static Program getInstance()
+        {
+            return new Program();
+        }
+
+        public int getCount()
+        {
+            return count;
+        }
+
+        public object getRef()
+        {
+            return this;
+        }
+
+      
+    }
+
+
+
+public static class AAA
+{
+    public static object GetRef(this object obj)
+    {
+        return obj;
+    }
+}
