5a0f7dbc07c8dabddfa18b919099ba9095bf1836
Scott Meyers <grb090423@qq.com>
2018-09-02 22:16:15 +0800

    feat: 新增精简的MVVM框架

---
+++ Assets/MVVM/BindableProperty.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * 可绑定属性
+ */
+
+
+
+namespace PD
+{
+
+	public class BindableProperty<T>
+	{
+
+		public delegate void ValueChangedHandler(T oldValue, T newValue);
+
+		public event ValueChangedHandler onValueChanged;
+
+		private T _value;
+
+		public T Value
+		{
+			get { return _value; }
+			set
+			{
+				if (!Equals(_value, value))
+				{
+					T old = _value;
+					_value = value;
+					OnValueChanged(old, _value);
+				}
+			}
+		}
+
+		private void OnValueChanged(T oldValue, T newValue)
+		{
+			onValueChanged?.Invoke(oldValue, newValue);
+		}
+
+		public override string ToString()
+		{
+			return Value != null ? Value.ToString() : "null";
+		}
+
+		public void Clear()
+		{
+			onValueChanged = null;
+		}
+
+	}
+
+}
---
+++ Assets/MVVM/PropertyBinder.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * 属性绑定器
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace PD
+{
+
+	public class PropertyBinder<T> where T : ViewModelBase
+	{
+
+		private delegate void BindHandler(T viewmodel);
+
+		private delegate void UnBindHandler(T viewmodel);
+
+		private readonly List<BindHandler> _binders = new List<BindHandler>();
+		private readonly List<UnBindHandler> _unbinders = new List<UnBindHandler>();
+
+		public void Add<TProperty>(string name, BindableProperty<TProperty>.ValueChangedHandler valueChangedHandler)
+		{
+			var fieldInfo = typeof(T).GetField(name, BindingFlags.Instance | BindingFlags.Public);
+			if (fieldInfo == null)
+			{
+				throw new Exception($"Unable to find bindableproperty field '{typeof(T).Name}.{name}'");
+			}
+
+			_binders.Add(viewmodel =>
+			{
+				GetPropertyValue<TProperty>(name, viewmodel, fieldInfo).onValueChanged += valueChangedHandler;
+			});
+
+			_unbinders.Add(viewModel =>
+			{
+				GetPropertyValue<TProperty>(name, viewModel, fieldInfo).onValueChanged -= valueChangedHandler;
+			});
+
+		}
+
+		private BindableProperty<TProperty> GetPropertyValue<TProperty>(string name, T viewModel, FieldInfo fieldInfo)
+		{
+			var value = fieldInfo.GetValue(viewModel);
+			BindableProperty<TProperty> bindableProperty = value as BindableProperty<TProperty>;
+			if (bindableProperty == null)
+			{
+				throw new Exception($"Illegal bindableproperty field '{typeof(T).Name}.{name}' ");
+			}
+
+			return bindableProperty;
+		}
+
+		public void Bind(T viewmodel)
+		{
+			if (viewmodel != null)
+			{
+				foreach (var binder in _binders)
+				{
+					binder(viewmodel);
+				}
+			}
+		}
+
+		public void Unbind(T viewmodel)
+		{
+			if (viewmodel != null)
+			{
+				foreach (var unbinder in _unbinders)
+				{
+					unbinder(viewmodel);
+				}
+			}
+		}
+
+	}
+
+}
