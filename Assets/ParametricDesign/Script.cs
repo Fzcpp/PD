527cca337902b6ef53ed775c505dc887ecb56fdd
Scott Meyers <grb090423@qq.com>
2018-09-08 21:35:53 +0800

    style: 排版

--- Assets/ParametricDesign/Script.cs
+++ Assets/ParametricDesign/Script.cs
@@ -15,103 +15,104 @@ namespace JL
 
 		public string Content;
 
-	    #region temp
+		#region temp
 
-	    private List<IDisposable> _disposables;
+		private List<IDisposable> _disposables;
 
-        #endregion
+		#endregion
 
-        public Script(IReactiveDictionary<string, Parameter> parameters)
+		public Script(IReactiveDictionary<string, Parameter> parameters)
 		{
 			_parameters = parameters;
-		    _disposables.Add( _parameters.ObserveAdd().Subscribe(OnAddParameter));
-		    _disposables.Add(_parameters.ObserveRemove().Subscribe(OnRemoveParameter));
-		    _disposables.Add(AoManager.Instance.AoPackages.ObserveAdd().Subscribe(OnAddAoPackage));
-		    _disposables.Add(AoManager.Instance.AoPackages.ObserveRemove().Subscribe(OnRemoveAoPackage));
-		    RegisterInitialize();
+			_disposables.Add(_parameters.ObserveAdd().Subscribe(OnAddParameter));
+			_disposables.Add(_parameters.ObserveRemove().Subscribe(OnRemoveParameter));
+			_disposables.Add(AoManager.Instance.AoPackages.ObserveAdd().Subscribe(OnAddAoPackage));
+			_disposables.Add(AoManager.Instance.AoPackages.ObserveRemove().Subscribe(OnRemoveAoPackage));
+			RegisterInitialize();
 		}
 
-	    private void RegisterInitialize()
-	    {
-	        foreach (var aoPackage in AoManager.Instance.AoPackages)
-            {
-                RegisterAoPackage(aoPackage);
-            }
+		private void RegisterInitialize()
+		{
+			foreach (var aoPackage in AoManager.Instance.AoPackages)
+			{
+				RegisterAoPackage(aoPackage);
+			}
 
-            foreach (var pair in _parameters)
-            {
-                RegisterParameter(pair.Key,pair.Value);
-            }
-        }
-
-        private void RegisterAoPackage(AoPackage aoPackage)
-        {
-            string getRef = "GetRef_" + aoPackage.Name;
-            _luaState.RegisterFunction(getRef, aoPackage,
-                aoPackage.GetType().GetMethod("GetRef"));
-            _luaState.DoString(aoPackage.Name + "=" + getRef + "()");
-        }
-
-	    private void RegisterParameter(string name, Parameter parameter)
-	    {
-	        string getRef = "GetRef_" + name;
-	        _luaState.RegisterFunction(getRef, parameter,
-	            parameter.GetType().GetMethod("GetRef"));
-	        _luaState.DoString(name + "=" + getRef + "()");
-	    }
-
-        private void UnregisterAoPackage(AoPackage aoPackage)
-	    {
-
-	    }
-
-	    private void UnregisterParameter(string name)
-	    {
-
-	    }
-
-	    private void OnAddParameter(DictionaryAddEvent<string,Parameter> parameterAddEvent)
-	    {
-            RegisterParameter(parameterAddEvent.Key,parameterAddEvent.Value);
-	    }
-
-	    private void OnRemoveParameter(DictionaryRemoveEvent<string, Parameter> parameterRemoveEvent)
-	    {
-
-	    }
-
-	    private void OnAddAoPackage(CollectionAddEvent<AoPackage> aoPackageAddEvent)
-	    {
-            RegisterAoPackage(aoPackageAddEvent.Value);
-	    }
-
-	    private void OnRemoveAoPackage(CollectionRemoveEvent<AoPackage> aoPackageRemoveEvent)
-	    {
-
-	    }
-
-	    public void Dispose()
-	    {
-	        foreach (var disposable in _disposables)
-	        {
-	            disposable.Dispose();
-            }
-	    }
-
-	    public void test(string name )
-	    {
-			
-	        string getRefFunc = "GetReference";
-	        Parameter parameter = _parameters[name];
-	        _luaState.RegisterFunction(getRefFunc, parameter, parameter.GetType().GetMethod(getRefFunc));
-
-            string objName = "obj"; // 对象名字为 name_obj 的形式
-
-            _luaState.DoString(objName+"= "+getRefFunc+"()");
-		    _luaState.DoString(objName + ":SetValue(122)");
-		    _luaState.DoString("first" + "= " + objName + ":GetValue()");
-		    _luaState.DoString(objName + ":SetValue(first + config:GetDelegate('GetSum2'):Invoke(100,100))");
+			foreach (var pair in _parameters)
+			{
+				RegisterParameter(pair.Key, pair.Value);
+			}
 		}
+
+		private void RegisterAoPackage(AoPackage aoPackage)
+		{
+			string getRef = "GetRef_" + aoPackage.Name;
+			_luaState.RegisterFunction(getRef, aoPackage,
+				aoPackage.GetType().GetMethod("GetRef"));
+			_luaState.DoString(aoPackage.Name + "=" + getRef + "()");
+		}
+
+		private void RegisterParameter(string name, Parameter parameter)
+		{
+			string getRef = "GetRef_" + name;
+			_luaState.RegisterFunction(getRef, parameter,
+				parameter.GetType().GetMethod("GetRef"));
+			_luaState.DoString(name + "=" + getRef + "()");
+		}
+
+		private void UnregisterAoPackage(AoPackage aoPackage)
+		{
+
+		}
+
+		private void UnregisterParameter(string name)
+		{
+
+		}
+
+		private void OnAddParameter(DictionaryAddEvent<string, Parameter> parameterAddEvent)
+		{
+			RegisterParameter(parameterAddEvent.Key, parameterAddEvent.Value);
+		}
+
+		private void OnRemoveParameter(DictionaryRemoveEvent<string, Parameter> parameterRemoveEvent)
+		{
+
+		}
+
+		private void OnAddAoPackage(CollectionAddEvent<AoPackage> aoPackageAddEvent)
+		{
+			RegisterAoPackage(aoPackageAddEvent.Value);
+		}
+
+		private void OnRemoveAoPackage(CollectionRemoveEvent<AoPackage> aoPackageRemoveEvent)
+		{
+
+		}
+
+		public void Dispose()
+		{
+			foreach (var disposable in _disposables)
+			{
+				disposable.Dispose();
+			}
+		}
+
+		public void test(string name)
+		{
+
+			string getRefFunc = "GetReference";
+			Parameter parameter = _parameters[name];
+			_luaState.RegisterFunction(getRefFunc, parameter, parameter.GetType().GetMethod(getRefFunc));
+
+			string objName = "obj"; // 对象名字为 name_obj 的形式
+
+			_luaState.DoString(objName + "= " + getRefFunc + "()");
+			_luaState.DoString(objName + ":SetValue(122)");
+			_luaState.DoString("first" + "= " + objName + ":GetValue()");
+			_luaState.DoString(objName + ":SetValue(first + config:GetDelegate('GetSum2'):Invoke(100,100))");
+		}
+
 		public void Execute()
 		{
 			_luaState.DoString(Content);
