d24049c32e9d8d75df1fad287dffd04b906b5aeb
sunzhaohao <sunzhaohenan@yahoo.com>
2018-09-17 22:28:43 +0800

    9-17

---
+++ Assets/_Playground/sunzhao/clipperTest/clipperTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ClipperLib;
+using UnityEngine;
+ 
+
+public class clipperTest : MonoBehaviour {
+static List<IntPoint> GetEllipsePoints(IntRect bounds)
+{
+    List < IntPoint > points=new List<IntPoint>();
+    points.Add(new IntPoint(bounds.left, bounds.bottom));
+
+        points.Add(new IntPoint(bounds.left,bounds.top));
+    points.Add(new IntPoint(bounds.right, bounds.top));
+    points.Add(new IntPoint(bounds.right,bounds.bottom));
+    
+    return points;
+}
+
+    public   Material material;
+    
+    void DrawPolygons(List<List<IntPoint>> p, Color color )
+ {
+      GL.PushMatrix();
+      material.SetPass(0);
+      GL.LoadOrtho();  
+      foreach (var points in p)
+        {  
+            GL.Begin(GL.QUADS);//绘制类型为四边形
+           //GL.Begin(GL.TRIANGLE_STRIP);
+            GL.Color(color);
+            Debug.Log(points[0].X+","+points[0].Y+";"+points[2].X+","+points[2].Y);
+          //  GL.Vertex3(x / Screen.width, y / Screen.height, 0);//['vɜ:teks] n. 最高点；顶点
+          //  GL.Vertex3(x / Screen.width, y2 / Screen.height, 0);
+           // GL.Vertex3( x2 / Screen.width,  y2/ Screen.height, 0);
+           // GL.Vertex3( x2 / Screen.width, y / Screen.height, 0);
+            for(int i=0;i<points.Count;i++)
+                GL.Vertex3(points[i].X / Screen.width, points[i].Y / Screen.height, 0);//['vɜ:teks] n. 最高点；顶点
+
+            GL.End();
+        }   
+      GL.PopMatrix();
+ }
+
+    private List<List<IntPoint>> subjs;
+    private List<List<IntPoint>> clips;
+    private List<List<IntPoint>> solution;
+    // Use this for initialization
+    private IntPoint GenerateRandomPoint(int l, int t, int r, int b, System.Random rand)
+    {
+        int Q = 10;
+        return new IntPoint(
+            Convert.ToInt64(  rand.Next(r / Q) * Q + l + 10     ),
+            Convert.ToInt64( rand.Next(b / Q) * Q + t + 10)   );
+    }
+    void Start ()
+    {
+        
+        subjs = new List<List<IntPoint>>(3);
+	    subjs.Add(GetEllipsePoints(new IntRect(100, 250, 300, 50)));
+	    subjs.Add(GetEllipsePoints(new IntRect(125, 130, 275, 80)));
+	    subjs.Add(GetEllipsePoints(new IntRect(125, 220, 275, 170)));
+
+        clips = new List<List<IntPoint>>(1);
+        clips.Add(GetEllipsePoints(new IntRect(140, 270, 220, 20)));
+
+        solution = new List<List<IntPoint>>();
+	    Clipper c = new Clipper();
+        c.AddPaths(subjs, PolyType.ptSubject, true);
+	    c.AddPaths(clips, PolyType.ptClip, true);
+        c.Execute(ClipType.ctIntersection, solution);
+        Debug.Log("Solution:");
+        foreach (var s in solution)
+        {
+            string info="";
+            for(int i=0;i<s.Count;i++)
+                info=info+s[i].X+","+s[i].Y+"  "  ;
+            Debug.Log(info);
+        }
+        Debug.Log("end"); 
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnRenderObject()
+    {
+        DrawPolygons(subjs, new Color(0x80, 0x33, 0xFF,0xFF));//0x8033FFFF));
+         DrawPolygons(clips, new Color(0x80,0xFF,0xFF,0x33));//0x80FFFF33
+        DrawPolygons(solution, new Color(0x40,0x80,0x80,0x80));//0x40808080
+    }
+}
---
+++ Assets/_Playground/sunzhao/clipperTest/test.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    private List<Vector3> linePos;
+             private bool startDraw = false;
+             private Vector3 oldMousePosition;
+         public float StepSize = 80f;//每次差值的步长
+          public float size = 60f;//每个单独模块的宽度
+           public Material LineMaterial;
+           public Color color = Color.red;
+             public float roud = 30;
+    // Use this for initialization
+    void Start () {
+        linePos = new List<Vector3>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	       oldMousePosition = Input.mousePosition;
+	       color = Color.red;
+	       startDraw = true;
+	    }
+	    if (Input.GetMouseButtonUp(0))
+	    {
+	       startDraw = false;
+	       linePos.Clear();//清理绘制点，不然每帧都会重新绘制
+	    }
+	    if (Input.GetMouseButtonDown(1))
+	    {
+	       oldMousePosition = Input.mousePosition;
+	       startDraw = true;
+	        color = Color.white;
+	    }
+	    if (Input.GetMouseButtonUp(1))
+	     {
+	         startDraw = false;
+	       linePos.Clear();//清理绘制点，不然每帧都会重新绘制
+	     }
+	    if (startDraw)
+	    {
+	        if (StepSize == 0) return;
+	        var newMousePosition = (Vector2)Input.mousePosition;
+	        if (Vector2.Distance(newMousePosition, oldMousePosition) < size) return;
+	        float stepCount = Vector2.Distance(oldMousePosition, newMousePosition) / StepSize + 1;
+	        for (int i = 0; i < stepCount; i++)
+	        {
+	            var subMousePosition = Vector3.Lerp(oldMousePosition, newMousePosition, i / stepCount);
+	            linePos.Add(subMousePosition);
+	        }
+	        oldMousePosition = newMousePosition;
+
+        }
+    }
+    public void OnRenderObject()
+    {
+         GL.PushMatrix();
+         LineMaterial.SetPass(0);
+         for (int i = 0; i<linePos.Count - 1; i++)
+         {
+             Vector3 startPos = linePos[i];
+             Vector3 endPos = linePos[i + 1];
+             DrawLineFun(startPos.x, startPos.y, endPos.x, endPos.y);
+         }
+         GL.PopMatrix();
+    }
+    void DrawLineFun(float x1, float y1, float x2, float y2)
+    {
+        DrawRect(x1 - roud, y1 - roud, 2 * roud, color);
+    }
+
+     void DrawRect(float x, float y, float width, Color myColor)
+     {
+         float height = width;
+         //绘制2D图像  
+         GL.LoadOrtho();
+         GL.Begin(GL.QUADS);
+         GL.Color(myColor);
+         GL.Vertex3(x / Screen.width, y / Screen.height, 0);
+         GL.Vertex3(x / Screen.width, (y + height) / Screen.height, 0);
+         GL.Vertex3((x + width) / Screen.width, (y + height) / Screen.height, 0);
+         GL.Vertex3((x + width) / Screen.width, y / Screen.height, 0);
+        GL.End();
+ 
+    }
+}
